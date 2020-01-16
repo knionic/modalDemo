7414c22b686e2bd207b20b22f148d6505ed453ff
rafiqkn <sonu.rafiq786@gmail.com>
2020-01-16 21:17:26 +0530

    its a demo app for pop over and modal page in ionic 4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  dataReturned: any;
 
-  constructor() {}
+  constructor(public modalController: ModalController) {}
 
+
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      componentProps: {
+        "paramID": 123,
+        "paramTitle": "Test Title"
+      }
+    });
+ 
+    modal.onDidDismiss().then((dataReturned) => {
+      if (dataReturned !== null) {
+        this.dataReturned = dataReturned.data;
+        //alert('Modal Sent Data :'+ dataReturned);
+      }
+    });
+ 
+    return await modal.present();
+  }
 }
+
+
